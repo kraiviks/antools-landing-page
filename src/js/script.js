5a9581c23e3762a84f71c4c6ae4d2e3a49195949
kraiviks <kraiviks@gmail.com>
2022-02-20 13:30:14 +0200

    added section slider

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+	$(".slider__wrapp").slick({
+		arrows: true,
+		prevArrow:
+			"<button type='button' class='slick-prev pull-left'><i class='fz50 link fa-solid fa-angle-left' aria-hidden='true'></i></button>",
+		nextArrow:
+			"<button type='button' class='slick-next pull-right'><i class='fz50 link fa-solid fa-angle-right' aria-hidden='true'></i></button>",
+	});
+});
